9be7efc42e60241c25a779f0afdf4e9b209954cf
leang27 <hangleang127@gmail.com>
2020-12-22 14:51:57 +0700

    LDE Leave CLI Opts

--- lde_leave/src/main.rs
+++ lde_leave/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+/// Easily triggering leave session actions: logout, reboot, shutdown, hibernate, and lock screen
+#[derive(StructOpt, Debug)]
+#[structopt(name = "LDE Leave")]
+struct LeaveOpt {
+
+    /// Logout.
+    #[structopt(long)]
+    logout: bool,
+
+    /// Lockscreen.
+    #[structopt(long)]
+    lockscreen: bool,
+
+    /// Suspend.
+    #[structopt(long)]
+    suspend: bool,
+
+    /// Hibernate.
+    #[structopt(long)]
+    hibernate: bool,
+
+    /// Shutdown.
+    #[structopt(long)]
+    shutdown: bool,
+
+    /// Reboot.
+    #[structopt(long)]
+    reboot: bool,
 }
+
+fn main() {
+    let opt = LeaveOpt::from_args();
+    println!("{:#?}", opt);
+}   
