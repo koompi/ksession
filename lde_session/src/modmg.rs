00f5b384806958e7ad9b58a735d73880ce15b8c0
veasnama <maveasnalearntosolve@gmail>
2021-01-20 16:02:02 +0700

    async session applicaiton

--- lde_session/src/modmg.rs
+++ lde_session/src/modmg.rs
@@ -1,18 +1,16 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 use crate::sm_xdg::xdg_autostart::desktop_files;
-use async_trait::async_trait;
 use std::fs;
 use std::path::PathBuf;
 use std::process::ExitStatus;
 use tokio::process::Command;
-#[async_trait]
 pub trait LDEModuleManager {
     fn set_window_manager(&mut self, wm_name: &str);
     fn start_process(&self, proc_name: &str);
     fn stop_process(&self, proc_name: &str);
     fn list_modlues(&mut self) -> Vec<String>;
-    async fn startup(&mut self);
+    fn startup(&mut self);
     fn logout(&self, can_exit: bool);
 }
 #[derive(Default)]
@@ -24,30 +22,32 @@ pub struct ModuleManager {
     // brief Window Manager command
     window_manager: String,
 }
-#[async_trait]
+
 impl LDEModuleManager for ModuleManager {
     fn set_window_manager(&mut self, wm_name: &str) {
         self.window_manager = wm_name.to_string();
     }
     fn start_process(&self, proc_name: &str) {
-        let mut child = Command::new(proc_name)
-            .spawn()
-            .expect("Failed to kick of auto start application");
-        tokio::spawn(async move {
-            let status = child
-                .wait()
-                .await
-                .expect("child process encountered an error");
+        match Command::new(proc_name).spawn() {
+            Ok(mut child) => {
+                tokio::spawn(async move {
+                    let status = child
+                        .wait()
+                        .await
+                        .expect("child process encountered an error");
 
-            println!("child status was: {}", status);
-        });
+                    println!("child status was: {}", status);
+                });
+            }
+            Err(e) => println!("error: {:?}", e),
+        }
     }
     fn stop_process(&self, proc_name: &str) {}
     fn list_modlues(&mut self) -> Vec<String> {
         Vec::<String>::new()
     }
-    async fn startup(&mut self) {
-        self.start_wm().await;
+    fn startup(&mut self) {
+        self.start_wm();
     }
     fn logout(&self, can_exit: bool) {}
 }
@@ -59,62 +59,44 @@ impl ModuleManager {
         }
     }
 
-    async fn start_wm(&mut self) -> &mut Self {
+    fn start_wm(&mut self) {
         if !get_wm().is_empty() {
             self.wm_started = true;
-            return self;
         } else {
-            let mut child = Command::new(&self.window_manager)
-                .arg("&")
-                .spawn()
-                .expect("Failed to start window manager");
-            tokio::spawn(async move {
-                let status = child
-                    .wait()
-                    .await
-                    .expect("child process encountered an error");
-
-                println!("child status was: {}", status);
-            });
+            self.start_process(&self.window_manager);
         }
         std::thread::sleep(std::time::Duration::from_millis(100));
-        self
     }
-    pub async fn wm_started(&mut self) {
-        println!("Window manager started");
+    pub fn wm_started(&mut self) {
         println!("window manager: {}", get_wm());
     }
-    pub async fn start_autostart(&mut self) -> &mut Self {
+    pub async fn start_autostart(&mut self) {
         let list_files = desktop_files();
 
         for file in list_files {
             let content = fs::read_to_string(&file).expect("Something wrong reading file");
             if content.contains("X-LDE-Module") {
-                println!("{:?}", file);
                 match self.start_app_process(file).await {
-                    Ok(()) => println!("run fine"),
+                    Ok(()) => println!("module started"),
                     Err(e) => println!("Error: {}", e),
                 }
             }
         }
-        self
     }
     async fn start_app_process(&mut self, file: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-        let entry = freedesktop_entry_parser::parse_entry(file).unwrap();
-        let binary = entry
-            .section("Desktop Entry")
-            .attr("Exec")
-            .expect("Attribute doesn't exist");
-        println!(" Executable: {}", binary);
-        let mut child = Command::new(binary).arg("&").spawn().expect("running");
-        tokio::spawn(async move {
-            let status = child
-                .wait()
-                .await
-                .expect("child process encountered an error");
-
-            println!("child status was: {}", status);
-        });
+        match freedesktop_entry_parser::parse_entry(file) {
+            Ok(entry) => match entry.section("Desktop Entry").attr("Exec") {
+                Some(binary) => {
+                    self.start_process(binary);
+                }
+                None => {
+                    eprintln!("Binary not found")
+                }
+            },
+            Err(e) => {
+                eprintln!("failed to parse desktop file: ")
+            }
+        }
         Ok(())
     }
     fn start_config_update(&mut self) {}
--- lde_session/src/sessionapp.rs
+++ lde_session/src/sessionapp.rs
@@ -1,9 +1,27 @@
-pub trait SessionApplication {
-    fn set_windowmanager();
-    fn set_config();
-    fn startup();
+#![allow(dead_code)]
+use crate::modmg::{LDEModuleManager, ModuleManager};
+#[derive(Default)]
+pub struct SessionApplication {
+    modmg: ModuleManager,
+    config_name: String,
+}
+impl SessionApplication {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+    pub fn set_windowmanager(&mut self, name: &str) {
+        self.modmg.set_window_manager(name);
+    }
+    fn set_config(&self) {}
+    pub async fn startup(&mut self) {
+        self.set_windowmanager("kwin_x11");
+        self.modmg.startup();
+        self.modmg.start_autostart().await;
+    }
 
-    fn load_enviromentsettings();
-    fn load_keyboardsettings();
-    fn load_mousesettings();
+    fn load_enviromentsettings(&mut self) {}
+    fn load_keyboardsettings(&mut self) {}
+    fn load_mousesettings(&mut self) {}
 }
